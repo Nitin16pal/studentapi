bc1f1eb1e4769ba37a0c48e0a2b8b5fc0490eb40
Nitin Pal <nitinkp1999@gmail.com>
2024-01-01 23:27:34 +0530

    laravel api uploads

---
+++ laravel-api/app/Http/Controllers/Api/StudentController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        $students = Student::all();
+        if ($students->count() > 0) {
+            return response()->json([
+                'status' => 200,
+                'students' => $students,
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => "Record Not Found",
+                'status' => 404,
+            ], 400);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:191',
+            'course' => 'required|string|max:191',
+            'email' => 'required|email|max:191',
+            'phone' => 'required|digits:10',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'message' => $validator->messages()
+            ], 422);
+        } else {
+            $student = Student::create([
+                'name' => $request->name,
+                'course' => $request->course,
+                'email' => $request->email,
+                'phone' => $request->phone,
+            ]);
+
+            if ($student) {
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Record created successfully'
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 500,
+                    'message' => 'Something went wrong'
+                ], 500);
+            }
+        }
+    }
+
+    public function show($id)
+    {
+        $student = Student::find($id);
+        if ($student) {
+            return response()->json([
+                'status' => 200,
+                'message' => $student
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => 'No Such Record Found'
+            ], 404);
+        }
+    }
+    public function edit(Request $request, int $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:191',
+            'course' => 'required|string|max:191',
+            'email' => 'required|email|max:191',
+            'phone' => 'required|digits:10',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'message' => $validator->messages()
+            ], 422);
+        } else {
+            $student = Student::find($id);
+
+
+            if ($student) {
+                $student->update([
+                    'name' => $request->name,
+                    'course' => $request->course,
+                    'email' => $request->email,
+                    'phone' => $request->phone,
+                ]);
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Record updated successfully'
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 500,
+                    'message' => 'No Record Found'
+                ], 500);
+            }
+        }
+    }
+
+    public function delete(int $id)
+    {
+        $student = Student::find($id);
+        if ($student) {
+            $student->delete();
+            return response()->json([
+                'status' => 200,
+                'message' => 'Record delete successfully'
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => 'No Record Found'
+            ], 404);
+        }
+    }
+}
